6edbd314c71ba5e4d3914b967386c19472a3fe87
Shryansh Parashar <Jaguar000212@gmail.com>
2025-03-27 07:11:05 +0530

    Added Algos
    Searching - Linear Search

---
+++ Algorithms/Searching/LinearSearch.cpp
@@ -0,0 +1,8 @@
+int LinearSearch(const int *arr, const int size, const int target) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            return i;
+        }
+    }
+    return -1;
+}
